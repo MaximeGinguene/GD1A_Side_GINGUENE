abc44f92a72de9043df8bc0f5a277ab3e8e2fd25
MaximeGinguene <118370269+MaximeGinguene@users.noreply.github.com>
2023-02-01 15:31:26 +0100

    Add files via upload

---
+++ Projet Side Scroller/assets/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="../../../Programmation Side scroller/Projet Side Scroller/assets/Tileset.png" width="96" height="96"/>
+</tileset>
